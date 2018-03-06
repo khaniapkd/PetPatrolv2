fff7eb0dda53896a3777c44320a8d6706fc33e69
paulinyohanesta <paulinlinda14@gmail.com>
2018-03-06 14:18:41 +0700

    layout forgot 2

--- app/src/main/java/com/petpatrol/petpatrolapp/ForgotPassword2.java
+++ app/src/main/java/com/petpatrol/petpatrolapp/ForgotPassword2.java
@@ -1,7 +1,9 @@
 package com.petpatrol.petpatrolapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ForgotPassword2 extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class ForgotPassword2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password2);
+    }
+
+    public void Back_login(View view) {
+        Intent intent = new Intent(ForgotPassword2.this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/petpatrol/petpatrolapp/MainActivity.java
+++ app/src/main/java/com/petpatrol/petpatrolapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.petpatrol.petpatrolapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    public void send_email(View view) {
+        Intent intent = new Intent(ForgotPassword.this,ForgotPassword2.class );
+        startActivity(intent);
     }
 }
