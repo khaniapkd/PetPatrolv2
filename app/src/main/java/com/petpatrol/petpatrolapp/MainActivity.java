3e263b5bf1ac798ccf2af7bcea43f795d3c8b5aa
wildanaditya <35674207+wildanaditya@users.noreply.github.com>
2018-03-06 15:39:59 +0700

    Login, dan Splash

--- app/src/main/java/com/petpatrol/petpatrolapp/MainActivity.java
+++ app/src/main/java/com/petpatrol/petpatrolapp/MainActivity.java
@@ -2,12 +2,42 @@ package com.petpatrol.petpatrolapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button login;
+    EditText username, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login=(Button)findViewById(R.id.btnlogin);
+        username=(EditText)findViewById(R.id.txtuser);
+        password=(EditText)findViewById(R.id.txtpass);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+    }
+
+    public void login(){
+        String user=username.getText().toString().trim();
+        String pass=password.getText().toString().trim();
+        if(user.equals("adam")&& pass.equals("adam"))
+        {
+            Toast.makeText(this,"username and password matched!",Toast.LENGTH_LONG).show();
+        }else {
+            Toast.makeText(this,"username and password do not matched!",Toast.LENGTH_LONG).show();
+        }
     }
 }
+
