391eb7dc6be3218af57bca0b4f9c3dc121ef148e
paulinyohanesta <paulinlinda14@gmail.com>
2018-03-06 13:18:59 +0700

    layout forgot 2

--- app/src/main/java/com/petpatrol/petpatrolapp/ForgotPassword2.java
+++ app/src/main/java/com/petpatrol/petpatrolapp/ForgotPassword2.java
@@ -3,13 +3,11 @@ package com.petpatrol.petpatrolapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ForgotPassword2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_forgot_password2);
     }
 }
