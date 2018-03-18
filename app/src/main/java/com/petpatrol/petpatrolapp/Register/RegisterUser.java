4c257e475f2f01123b1cbd3f8d688a7ce204772c
gonogungun <margonoharry987@gmail.com>
2018-03-18 16:38:39 +0700

    Merubah xml register user, dan menambah registeruser.java dan xml petcare

---
+++ app/src/main/java/com/petpatrol/petpatrolapp/Register/RegisterUser.java
@@ -0,0 +1,15 @@
+package com.petpatrol.petpatrolapp.Register;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.petpatrol.petpatrolapp.R;
+
+public class RegisterUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_user);
+    }
+}
