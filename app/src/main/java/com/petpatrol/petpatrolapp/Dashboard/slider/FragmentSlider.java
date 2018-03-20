b8f871751b64020375d9041e079f0ff062c3d021
khaniapkd <putrikhania20@gmail.com>
2018-03-21 00:04:58 +0700

    Slider baru setengah jadi

--- app/src/main/java/com/petpatrol/petpatrolapp/Dashboard/slider/FragmentSlider.java
+++ app/src/main/java/com/petpatrol/petpatrolapp/Dashboard/slider/FragmentSlider.java
@@ -1,8 +1,38 @@
 package com.petpatrol.petpatrolapp.Dashboard.slider;
 
-/**
- * Created by ASUS on 3/20/2018.
- */
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
-public class FragmentSlider {
+import com.petpatrol.petpatrolapp.R;
+import com.bumptech.glide.Glide;
+
+
+public class FragmentSlider extends Fragment {
+    private static final String ARG_PARAM1 = "params";
+
+    private String imageUrls;
+
+    public FragmentSlider(){
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        imageUrls = getArguments().getString(ARG_PARAM1);
+        View view = inflater.inflate(R.layout.fragment_slider_item, container, false);
+        ImageView img = (ImageView) view.findViewById(R.id.img);
+        Glide.with(getActivity()).load(imageUrls).into(img);
+        return view;
+    }
+
+    public static FragmentSlider newInstance(String params){
+        FragmentSlider fragment = new FragmentSlider();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, params);
+        fragment.setArguments(args);
+        return fragment;
+    }
 }
