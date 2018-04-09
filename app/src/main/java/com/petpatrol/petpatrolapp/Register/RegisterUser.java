b800f6199fb57fab39e91dc31cb5e840c2c1da7d
gonogungun <margonoharry987@gmail.com>
2018-04-09 14:04:49 +0700

    Register user menggunakan auth dari firebase

--- app/src/main/java/com/petpatrol/petpatrolapp/Register/RegisterUser.java
+++ app/src/main/java/com/petpatrol/petpatrolapp/Register/RegisterUser.java
@@ -1,74 +1,94 @@
 package com.petpatrol.petpatrolapp.Register;
 
-import android.nfc.Tag;
-import android.os.Bundle;
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.EditText;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.petpatrol.petpatrolapp.R;
+import com.google.firebase.auth.AuthResult;
 
-import java.util.HashMap;
-import java.util.Map;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+import com.petpatrol.petpatrolapp.Login.MainActivity;
+import com.petpatrol.petpatrolapp.R;
+import com.petpatrol.petpatrolapp.config.Constant;
 
 public class RegisterUser extends AppCompatActivity {
 
-    public static final String USERNAME_KEY = "username";
-    public static final String EMAIL_KEY = "email";
-    public static final String SANDI_KEY = "password";
-   // public static final String REPASSWORD_KEY = "repassword";
-
-    private DocumentReference mDocRef = FirebaseFirestore.getInstance().document("PetPatrol/UserData");
+    //Deklarasi view
+    @BindView(R.id.email) //@BindView declare sekaligus inisialisasi view dengan menggunakan library ButterKnife
+            TextInputEditText tvEmail;
+    @BindView(R.id.password) //@BindView declare sekaligus inisialisasi view dengan menggunakan library ButterKnife
+            TextInputEditText tvPassword;
+    @BindView(R.id.button2) //@BindView declare sekaligus inisialisasi view dengan menggunakan library ButterKnife
+            Button btnRegister;
+    private ProgressDialog pbDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_user);
+        ButterKnife.bind(this); //Binding ButterKnife dengan activity ini
+        pbDialog = new ProgressDialog(this);
     }
 
+    //method handling btnregister
+    @OnClick(R.id.button2)
+    public void register() {
+        //validasi kosong
+        if(tvEmail.getText().toString().isEmpty()) {
+            tvEmail.setError("Required");
+            return;
+        }
+        //validasi kosong
+        if(tvPassword.getText().toString().isEmpty()) {
+            tvPassword.setError("Required");
+            return;
+        }
 
-    public void saveDataUser(View view) {
-        EditText usrUsername = findViewById(R.id.usr_usrname);
-        EditText usrEmail = findViewById(R.id.usr_usremail);
-        EditText usrSandi = findViewById(R.id.usr_sandi);
-        //EditText usrRePassword = findViewById(R.id.usr_usrre_password);
+        pbDialog.setMessage("Proses Mendaftarkan");
+        pbDialog.setIndeterminate(true);
+        pbDialog.show();
 
-        String UsernameText = usrUsername.getText().toString();
-        String EmailText = usrEmail.getText().toString();
-        String SandiText = usrSandi.getText().toString();
-        //String RePasswordText = usrRePassword.getText().toString();
-
-        if (UsernameText.isEmpty() || EmailText.isEmpty()) { return;} // || PasswordText.isEmpty() || RePasswordText.isEmpty()
-
-        Map<String, Object> dataToSave = new HashMap<String, Object>();
-
-        dataToSave.put(USERNAME_KEY,UsernameText);
-        dataToSave.put(EMAIL_KEY,EmailText);
-        dataToSave.put(SANDI_KEY,SandiText);
-        //dataToSave.put(REPASSWORD_KEY,usrRePassword);
-
-        mDocRef.set(dataToSave).addOnCompleteListener(new OnCompleteListener<Void>() {
-            @Override
-            public void onComplete(@NonNull Task<Void> task) {
-                if (task.isSuccessful()) {
-                    Toast.makeText(RegisterUser.this, "Document has been saved", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(RegisterUser.this, "Document has not been saved", Toast.LENGTH_SHORT).show();
-                }
-            }
-
-        });
-
+        createUser();
     }
 
+    //method untuk register user, dengan fitur email authentication di firebase
+    private void createUser() {
+        final String email = tvEmail.getText().toString();
+        String password = tvPassword.getText().toString();
 
+        Constant.mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            pbDialog.dismiss();
+                            Log.d("", "createUserWithEmail:success");
+                            Constant.currentUser = Constant.mAuth.getCurrentUser(); //simpan data user
+                            Toast.makeText(RegisterUser.this, "Berhasil mendaftar, Silahkan login!", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(RegisterUser.this, MainActivity.class)); //panggil login activity
+                            finish();
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            pbDialog.dismiss();
+                            Log.w("", "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(RegisterUser.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+                    }
+                });
+    }
 }
