975bf7403824744c0ba2654d85978bc833e00df4
khaniapkd <putrikhania20@gmail.com>
2018-04-16 12:33:46 +0700

    Menu My Pet

--- app/src/main/java/com/petpatrol/petpatrolapp/Login/MainActivity.java
+++ app/src/main/java/com/petpatrol/petpatrolapp/Login/MainActivity.java
@@ -1,134 +1,163 @@
 package com.petpatrol.petpatrolapp.Login;
 
-import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.res.ColorStateList;
+import android.content.res.XmlResourceParser;
 import android.os.Bundle;
-import android.text.TextUtils;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.text.InputType;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
-import com.petpatrol.petpatrolapp.ForgotPassword.ForgotPassword2;
 import com.petpatrol.petpatrolapp.R;
-import com.petpatrol.petpatrolapp.Register.PetCareRegisterActivity;
-import com.petpatrol.petpatrolapp.SignOut.SignOut;
 
-import java.security.AccessController;
-import java.security.Provider;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Fragment implements OnClickListener {
+    private static View view;
 
-    private EditText mEmail;
-    private EditText mPassword;
-    private Button mlogin;
-
-
-    private FirebaseAuth mAuth;
-    private FirebaseAuth.AuthStateListener mAuthListener;
-
+    private static EditText emailid, password;
+    private static Button loginButton;
+    private static TextView forgotPassword, signUp;
+    private static CheckBox show_hide_password;
+    private static LinearLayout loginLayout;
+    private static Animation shakeAnimation;
+    private static FragmentManager fragmentManager;
+    
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-
-        mAuth = FirebaseAuth.getInstance();
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-        mEmail = (EditText) findViewById(R.id.email);
-        mPassword = (EditText) findViewById(R.id.password);
-        mlogin = (Button) findViewById(R.id.login);
-
-
-
-        mAuth = FirebaseAuth.getInstance();
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
-
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-
-                if (firebaseAuth.getCurrentUser() != null) {
-
-                    startActivity(new Intent(MainActivity.this, SignOut.class) );
-                }
-            }
-        };
-        mlogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SignIn();
-            }
-        });
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.activity_main, container, false);
+        initViews();
+        setListeners();
+        return view;
     }
 
-    private void SignIn() {
+    // Initiate Views
+    private void initViews() {
+        fragmentManager = getActivity().getSupportFragmentManager();
 
-        String email = mEmail.getText().toString();
-        String password = mPassword.getText().toString();
+        emailid = (EditText) view.findViewById(R.id.login_emailid);
+        password = (EditText) view.findViewById(R.id.login_password);
+        loginButton = (Button) view.findViewById(R.id.loginBtn);
+        forgotPassword = (TextView) view.findViewById(R.id.forgot_password);
+        signUp = (TextView) view.findViewById(R.id.createAccount);
+        show_hide_password = (CheckBox) view.findViewById(R.id.show_hide_password);
+        loginLayout = (LinearLayout) view.findViewById(R.id.login_layout);
 
-        if (TextUtils.isEmpty(email) | TextUtils.isEmpty(password)) {
-            Toast.makeText(MainActivity.this, "Harap isi Email dan Password Anda", Toast.LENGTH_SHORT).show();
+        // Load ShakeAnimation
+        shakeAnimation = AnimationUtils.loadAnimation(getActivity(),
+                R.anim.shake);
 
-        } else {
+        // Setting text selector over textviews
+        @SuppressLint("ResourceType") XmlResourceParser xrp = getResources().getXml(R.drawable.text_selector);
+        try {
+            ColorStateList csl = ColorStateList.createFromXml(getResources(),
+                    xrp);
 
-            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener() {
+            forgotPassword.setTextColor(csl);
+            show_hide_password.setTextColor(csl);
+            signUp.setTextColor(csl);
+        } catch (Exception e) {
+        }
+    }
 
-                @Override
+    // Set Listeners
+    private void setListeners() {
+        loginButton.setOnClickListener(this);
+        forgotPassword.setOnClickListener(this);
+        signUp.setOnClickListener(this);
 
-                public void onComplete(@NonNull Task task) {
-                    if (!task.isSuccessful()) {
-                        Toast.makeText(MainActivity.this, "Login Error", Toast.LENGTH_SHORT).show();
+        // Set check listener over checkbox for showing and hiding password
+        show_hide_password
+                .setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+                    @Override
+                    public void onCheckedChanged(CompoundButton button,
+                                                 boolean isChecked) {
+
+                        // If it is checkec then show password else hide
+                        // password
+                        if (isChecked) {
+
+                            show_hide_password.setText(R.string.hide_pwd);// change
+                            // checkbox
+                            // text
+
+                            password.setInputType(InputType.TYPE_CLASS_TEXT);
+                            password.setTransformationMethod(HideReturnsTransformationMethod
+                                    .getInstance());// show password
+                        } else {
+                            show_hide_password.setText(R.string.show_pwd);// change
+                            // checkbox
+                            // text
+
+                            password.setInputType(InputType.TYPE_CLASS_TEXT
+                                    | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                            password.setTransformationMethod(PasswordTransformationMethod
+                                    .getInstance());// hide password
+
+                        }
+
                     }
-                }
-            });
-        }
+                });
     }
 
     @Override
-
-    protected void onStart() {
-        super.onStart();
-        mAuth.addAuthStateListener(mAuthListener);
-
-    }
-
-
-    public void SignUp(View view) {
-        Intent intent = new Intent(MainActivity.this, PetCareRegisterActivity.class);
-        startActivity(intent);
-    }
-
-    public void forgot(View view) {
-        Intent intent = new Intent(MainActivity.this, ForgotPassword2.class);
-        startActivity(intent);
-    }
-
-    public void loginFB(View view) {
-        Intent intent = new Intent(MainActivity.this, SignOut.class);
-        startActivity(intent);
-    }
-
-    public final class JSSEProvider extends Provider {
-
-        public JSSEProvider() {
-            super("HarmonyJSSE", 1.0, "Harmony JSSE Provider");
-            AccessController.doPrivileged(new java.security.PrivilegedAction<Void>() {
-                public Void run() {
-                    put("SSLContext.TLS",
-                            "org.apache.harmony.xnet.provider.jsse.SSLContextImpl");
-                    put("Alg.Alias.SSLContext.TLSv1", "TLS");
-                    put("KeyManagerFactory.X509",
-                            "org.apache.harmony.xnet.provider.jsse.KeyManagerFactoryImpl");
-                    put("TrustManagerFactory.X509",
-                            "org.apache.harmony.xnet.provider.jsse.TrustManagerFactoryImpl");
-                    return null;
-                }
-            });
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.loginBtn:
+                checkValidation();
+                break;
         }
+
+    }
+
+    // Check Validation before login
+    private void checkValidation() {
+        // Get email id and password
+        String getEmailId = emailid.getText().toString();
+        String getPassword = password.getText().toString();
+
+        // Check patter for email id
+        Pattern p = Pattern.compile(Utils.regEx);
+
+        Matcher m = p.matcher(getEmailId);
+
+        // Check for both field is empty or not
+        if (getEmailId.equals("") || getEmailId.length() == 0
+                || getPassword.equals("") || getPassword.length() == 0) {
+            loginLayout.startAnimation(shakeAnimation);
+            new CustomToast().Show_Toast(getActivity(), view,
+                    "Enter both credentials.");
+
+        }
+        // Check if email id is valid or not
+        else if (!m.find())
+            new CustomToast().Show_Toast(getActivity(), view,
+                    "Your Email Id is Invalid.");
+            // Else do login and do your stuff
+        else
+            Toast.makeText(getActivity(), "Do Login.", Toast.LENGTH_SHORT)
+                    .show();
+
     }
 }
